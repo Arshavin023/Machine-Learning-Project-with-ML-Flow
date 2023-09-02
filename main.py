4e79032284f804f25e494eb428e8d26cf47d0fc7
Arshavin023 <uchejudennodim@gmail.com>
2023-09-02 19:09:51 +0100

    model trainer added

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME_01 = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME_01} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME_01} completed <<<<<<\n\nx=========x")
 except Exception as e:
     logger.exception(e)
     raise e
 
 STAGE_NAME_02 = "Data Validation stage"
+
 try:
     logger.info(f">>>>>> stage {STAGE_NAME_02} started <<<<<<")
-    obj = DataValidationTrainingPipeline()
-    obj.main()
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>> stage {STAGE_NAME_02} completed <<<<<<\n\nx=========x")
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME_03 = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME_03} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME_03} completed <<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME_04 = "Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME_04} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME_04} completed <<<<<< \n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -15,3 +15,20 @@ class DataValidationConfig:
     unzip_data_dir: Path
     all_schema: dict
 
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    alpha: float # will be read from params.yaml
+    l1_ratio: float # will be read from params.yaml 
+    target_column: str # will be read from schema.yaml 
+
+
+
